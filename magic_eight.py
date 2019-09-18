069795da18bde768b6d2fa22025c1734d57a4c2d
Gina Genova <ginagenova@0587385726.wireless.umich.net>
2019-09-18 18:21:05 -0400

    check question

--- magic_eight.py
+++ magic_eight.py
@@ -1,2 +1,16 @@
 def ask_question():
     question = input("What is your question?")
+    check_question(question)
+
+
+
+
+
+
+def check_question(question):
+    if question[-1] is "?":
+        answer_question()
+    elif question is "quit" or "Quit" or "quit." or "Quit.":
+        print("Talk to you later.")
+    else:
+        ask_question()
